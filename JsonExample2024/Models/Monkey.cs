e18a33492fcb669408d66663b8eb05b726432a91
User <User@LAB3-3>
2024-03-27 09:38:57 +0200

    up

--- JsonExample2024/Models/Monkey.cs
+++ JsonExample2024/Models/Monkey.cs
@@ -6,23 +6,25 @@ using System.Threading.Tasks;
 
 namespace JsonExample2024.Models
 {
-
-    public class MonkeyList
-    {
-        public List<Monkey> Monkeys { get; set; }
-    }
-
     public class Monkey
     {
         public string Name { get; set; }
         public string Location { get; set; }
         public string Details { get; set; }
-        public string Image { get; set; }
         public int Population { get; set; }
-        public float Latitude { get; set; }
-        public float Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public override string ToString()
+        {
+            return $@"
+Name: {Name}
+Location: {Location}
+Details:{Details}
+Population:{Population}
+Latitude:{Latitude}
+Longitude:{Longitude}
+";
+        }
     }
-
-
-
 }
---
+++ JsonExample2024/Models/MonkeyList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonExample2024.Models
+{
+    public class MonkeyList
+    {
+        public List<Monkey> Monkeys { get; set; }
+
+        public override string ToString()
+        {
+            string monkeystext = "";
+            foreach (var monkey in Monkeys)
+            {
+                monkeystext += $"{monkey}\n";
+            }
+            return monkeystext;
+        }
+    }
+}
